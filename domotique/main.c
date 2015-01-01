957fd0ddf19a95ceb5073093dabdbe59ea8972a8
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-01 12:58:38 +0000

    Update main.c
    move declaration of get_ip_address from this module to ip folder

--- domotique/main.c
+++ domotique/main.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 
-#include <unistd.h>
-#include <string.h> /* for strncpy */
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <netinet/in.h>
-#include <net/if.h>
-#include <arpa/inet.h>
+#include "ip.h"
 
 #include <wiringPi.h>
 #include <lcd.h>
@@ -38,7 +30,6 @@ int  ip_hundler;
 struct ifreq ifr;
 char *ip_address;
 
-void get_ip_address(char **ip);
 
 int main(){
 
@@ -102,23 +93,4 @@ digitalWrite (LCD_A, LOW) ;
 
 }
 return 0;
-}
-
-
-
-
-void get_ip_address(char **ip){
-
- ip_hundler = socket(AF_INET, SOCK_DGRAM, 0);
-
- /* I want to get an IPv4 IP address */
- ifr.ifr_addr.sa_family = AF_INET;
- /* I want IP address attached to "eth0" */
- strncpy(ifr.ifr_name, "wlan0", IFNAMSIZ-1);
- ioctl(ip_hundler, SIOCGIFADDR, &ifr);
- close(ip_hundler);
-
- *ip = inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr);
-
- printf("IP address on wlan0: %s\n", *ip);
 }
