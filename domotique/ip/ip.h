0683ef4ebc65bac35ad0120f50d815c38b6bb8d3
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-01 12:57:25 +0000

    Create ip.h

---
+++ domotique/ip/ip.h
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <string.h> /* for strncpy */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
+void get_ip_address(char **ip);
