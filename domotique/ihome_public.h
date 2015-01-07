c2b074bcbc449607a2afeb9d63022529eb6f88b9
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-07 16:48:16 +0000

    Update ihome_public.h
    define the boolean type
    add 2 new types (object) and 2 array (of objects) for communication between tasks : between read and update, and between update and write

--- domotique/ihome_public.h
+++ domotique/ihome_public.h
@@ -13,10 +13,61 @@
 *  Written by  : E. EL FAKIR                                 Date : 07/01/2015 *
 *                                                                              *
 ********************************************************************************
+#include <pthread.h>
 
+/**********************/
+/* Type Declaration   */
+/**********************/
+// Define the boolean type
+typedef unsigned char boolean_t;
+#define TRUE ( 1 == 1 )
+#define FALSE ( 1 == 0 )
+
+// define the structure of input object
+typedef struct input_object_s{
+  boolean_t       value ;
+  boolean_t       validity ;
+  pthread_mutex_t mutex ;
+} input_object_t ;
+
+typedef enum input_element_e{
+  input_BUTTON_1,
+  nb_Of_Input_Elements
+} input_element_t ;
+
+// define the structure of output object
+typedef struct output_object_s{
+  boolean_t       value ;
+  boolean_t       validity ;
+  pthread_mutex_t mutex ;
+} output_object_t ;
+
+typedef enum output_element_e{
+  output_RLY_LAMP_1,
+  output_RLY_LAMP_2,
+  output_RLY_LAMP_3,
+  output_RLY_LAMP_4,
+  nb_Of_Output_Elements
+} output_element_t ;
+
+/**********************/
+/* Public Data        */
+/**********************/
+// Array of input elements.
+extern input_object_t inputs_Array_Of_Elements [ nb_Of_Input_Elements ];
+extern const input_object_t input_object_cst;
+
+// Array of output elements.
+extern output_object_t outputs_Array_Of_Elements [ nb_Of_Output_Elements ];
+extern const output_object_t output_object_cst;
+
+/**********************/
+/* Public Functions   */
+/**********************/
 
 extern void *ihome_initialize ( void ) ;
 extern void *ihome_monitor    ( void ) ;
 extern void *ihome_read       ( void ) ;
 extern void *ihome_update     ( void ) ;
 extern void *ihome_write      ( void ) ;
+
