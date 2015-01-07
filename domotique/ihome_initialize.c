a85b7871b8555377758a434608ddf3ec719b38ad
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-07 15:00:32 +0000

    Create ihome_initialize.c

---
+++ domotique/ihome_initialize.c
@@ -0,0 +1,23 @@
+/*******************************************************************************
+* Copyright FEZ EMBEDDED SYSTEMS INDUSTRY (c) 2015       All Rights Reserved   *
+********************************************************************************/
+
+/*******************************************************************************
+* PROJECT : Intelligent Home (Raspberry Pi B+)                                 *
+********************************************************************************
+*                                                                              *
+*  Module Name : ihome_initialize                                              *
+*                                                                              *
+*  Description : initialize all software's datas.                              *
+*                                                                              *
+*  Written by  : E. EL FAKIR                                 Date : 07/01/2015 *
+*                                                                              *
+********************************************************************************
+
+#include "ihome_public.h"
+
+
+void *ihome_initialize ( void )
+{
+
+}
