0cd498e3a3526217e74f50ab6dcb3f06b0cb7d03
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-07 15:04:39 +0000

    Create ihome_write.c

---
+++ domotique/ihome_write.c
@@ -0,0 +1,22 @@
+/*******************************************************************************
+* Copyright FEZ EMBEDDED SYSTEMS INDUSTRY (c) 2015       All Rights Reserved   *
+********************************************************************************/
+
+/*******************************************************************************
+* PROJECT : Intelligent Home (Raspberry Pi B+)                                 *
+********************************************************************************
+*                                                                              *
+*  Module Name : ihome_write                                                   *
+*                                                                              *
+*  Description : write all outputs from array into MCU registers.              *
+*                                                                              *
+*  Written by  : E. EL FAKIR                                 Date : 07/01/2015 *
+*                                                                              *
+********************************************************************************
+
+#include "ihome_public.h"
+
+void *ihome_write ( void )
+{
+
+}
