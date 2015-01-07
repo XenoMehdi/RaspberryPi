5d31588439997629e179e722924124f0e9734e90
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-07 15:06:26 +0000

    Create ihome_update.c

---
+++ domotique/ihome_update.c
@@ -0,0 +1,22 @@
+/*******************************************************************************
+* Copyright FEZ EMBEDDED SYSTEMS INDUSTRY (c) 2015       All Rights Reserved   *
+********************************************************************************/
+
+/*******************************************************************************
+* PROJECT : Intelligent Home (Raspberry Pi B+)                                 *
+********************************************************************************
+*                                                                              *
+*  Module Name : ihome_update                                                  *
+*                                                                              *
+*  Description : update outputs array depending on inputs array.               *
+*                                                                              *
+*  Written by  : E. EL FAKIR                                 Date : 07/01/2015 *
+*                                                                              *
+********************************************************************************
+#include "ihome_public.h"
+
+void *ihome_update ( void )
+
+{
+
+}
