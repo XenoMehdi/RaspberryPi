90f8067f4974f9d647c63e1445e224ce7f9bbc0c
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-07 15:08:19 +0000

    Create ihome_monitor.c

---
+++ domotique/ihome_monitor.c
@@ -0,0 +1,22 @@
+/*******************************************************************************
+* Copyright FEZ EMBEDDED SYSTEMS INDUSTRY (c) 2015       All Rights Reserved   *
+********************************************************************************/
+
+/*******************************************************************************
+* PROJECT : Intelligent Home (Raspberry Pi B+)                                 *
+********************************************************************************
+*                                                                              *
+*  Module Name : ihome_monitor                                                 *
+*                                                                              *
+*  Description : send monitoring message to IoT server.                        *
+*                                                                              *
+*  Written by  : E. EL FAKIR                                 Date : 07/01/2015 *
+*                                                                              *
+********************************************************************************
+#include "ihome_public.h"
+
+void *ihome_monitor ( void )
+
+{
+
+}
