3737469e8657f91f1d5333364e973f7f559ccbc9
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-07 15:13:16 +0000

    Create ihome_public.h

---
+++ domotique/ihome_public.h
@@ -0,0 +1,22 @@
+/*******************************************************************************
+* Copyright FEZ EMBEDDED SYSTEMS INDUSTRY (c) 2015       All Rights Reserved   *
+********************************************************************************/
+
+/*******************************************************************************
+* PROJECT : Intelligent Home (Raspberry Pi B+)                                 *
+********************************************************************************
+*                                                                              *
+*  Module Name : ihome_public                                                  *
+*                                                                              *
+*  Description : API for iHome Software.                                       *
+*                                                                              *
+*  Written by  : E. EL FAKIR                                 Date : 07/01/2015 *
+*                                                                              *
+********************************************************************************
+
+
+extern void *ihome_initialize ( void ) ;
+extern void *ihome_monitor    ( void ) ;
+extern void *ihome_read       ( void ) ;
+extern void *ihome_update     ( void ) ;
+extern void *ihome_write      ( void ) ;
