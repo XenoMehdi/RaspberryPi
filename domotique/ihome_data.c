488144d6d7d549aa11a426eb2e7b0e96707bd06a
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-07 17:53:06 +0000

    Update ihome_data.c

--- domotique/ihome_data.c
+++ domotique/ihome_data.c
@@ -13,4 +13,15 @@
 *  Written by  : E. EL FAKIR                                 Date : 07/01/2015 *
 *                                                                              *
 ********************************************************************************
+#include "ihome_public.h"
 
+/**********************/
+/* Public Data        */
+/**********************/
+// Array of input elements.
+input_object_t inputs_Array_Of_Elements [ nb_Of_Input_Elements ];
+const input_object_t input_object_cst;
+
+// Array of output elements.
+output_object_t outputs_Array_Of_Elements [ nb_Of_Output_Elements ];
+const output_object_t output_object_cst;
