dc7c5e4e74141fee32366235a1d9dfcdd2449c41
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-08 11:14:07 +0000

    Update ihome_public.h

--- domotique/ihome_public.h
+++ domotique/ihome_public.h
@@ -22,6 +22,7 @@
 typedef unsigned char boolean_t;
 #define TRUE ( 1 == 1 )
 #define FALSE ( 1 == 0 )
+#define nb_OF_ACTIVE_MESSAGES 5
 
 // define the structure of input object
 typedef struct input_object_s{
@@ -61,28 +62,34 @@ typedef struct monitor_message_s{
   char message [255] ;
 } monitor_message_t ;
 
+typedef enum messages_elements_e{
+  MESSAGE_1,
+  MESSAGE_2,
+  MESSAGE_3,
+  MESSAGE_4,
+  MESSAGE_5,
+  MESSAGE_6,
+  MESSAGE_7,
+  MESSAGE_8,
+  MESSAGE_9,
+  MESSAGE_10,
+  nb_Of_Messages
+} messages_elements_t ;
+
 /* define a structure of message that contains a field for lcd_message      */
 /* and a field for monitoring_message with the same meaning but with        */
 /* different density, monitoring_message is more detailled than lcd_message */
-typedef struct messages_list_s{
-  unsigned int      id_message ;
-  lcd_message_t     lcd_message ;
-  monitor_message_t monitor_message ;
-} messages_list_t ;
+typedef struct messages_object_s{
+  messages_elements_t   id_message ;
+  lcd_message_t         lcd_message ;
+  monitor_message_t     monitor_message ;
+} messages_object_t ;
 
-typedef enum messages_elements_e{
-  MESSAGE_1,
-  MESSAGE_1,
-  MESSAGE_1,
-  MESSAGE_1,
-  MESSAGE_1,
-  MESSAGE_1,
-  MESSAGE_1,
-  MESSAGE_1,
-  MESSAGE_1,
-  MESSAGE_1,
-  nb_Of_Messages
-}
+typedef struct active_message_s{
+  messages_elements_t   id_message ;
+  boolean_t             printed_to_lcd ;
+  boolean_t             sent_to_server ;
+} active_message_t ;
 /**********************/
 /* Public Data        */
 /**********************/
@@ -95,8 +102,11 @@ extern output_object_t outputs_Array_Of_Elements [ nb_Of_Output_Elements ];
 extern const output_object_t output_object_cst;
 
 // List of defined messages 
-extern const messages_list_t messages_list [ nb_Of_Messages ] ;
+extern const messages_object_t messages_list_cst [ nb_Of_Messages ] ;
 
+// list of active messages to send to server and to print on LCD
+extern active_message_t active_message_list [ nb_OF_ACTIVE_MESSAGES] ;
+extern const active_message_t active_message_default_cst ;
 /**********************/
 /* Public Functions   */
 /**********************/
