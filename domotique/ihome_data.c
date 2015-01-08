b4d8ccc4b4e301af08ea0c1b704527ce829f181b
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-08 11:14:04 +0000

    Update ihome_data.c

--- domotique/ihome_data.c
+++ domotique/ihome_data.c
@@ -20,8 +20,17 @@
 /**********************/
 // Array of input elements.
 input_object_t inputs_Array_Of_Elements [ nb_Of_Input_Elements ];
-const input_object_t input_object_cst;
+const input_object_t input_object_cst   = { FALSE, FALSE, PTHREAD_MUTEX_INITIALIZER } ;
 
 // Array of output elements.
 output_object_t outputs_Array_Of_Elements [ nb_Of_Output_Elements ];
-const output_object_t output_object_cst;
+const output_object_t output_object_cst = { FALSE, FALSE, PTHREAD_MUTEX_INITIALIZER } ;
+
+// List of defined messages 
+const messages_object_t messages_list_cst [ nb_Of_Messages ] = {
+                {MESSAGE_1, {"software init","successfully"}, "Initialization of Software's data success." }
+};
+
+// list of active messages to send to server and to print on LCD
+active_message_t active_message_list [ nb_OF_ACTIVE_MESSAGES] ;
+const active_message_t active_message_default_cst = {0, FALSE, FALSE} ;
