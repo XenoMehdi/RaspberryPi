2c3ac3a617dbf8699fa1f180e642c7d4bf6554f6
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-09 11:02:21 +0000

    Create ihome_write_lcd.c

---
+++ domotique/ihome_write_lcd.c
@@ -0,0 +1,42 @@
+/**
+ *	write to lcd the message passed as parameter.
+ *
+ *  @Module   ihome_write_lcd
+ *  @author 	El Mehdi El Fakir
+ *  @email		elmehdi@elfakir.me
+ *  @website	--
+ *  @link		  --
+ *  @version 	v1.0
+ *  @compiler GCC
+ *  @hardware Raspberry Pi B+
+ *  @license	GNU GPL v3
+ *	
+ * |----------------------------------------------------------------------
+ * | Copyright (C) FEZ EMBEDDED SYSTEMS INDUSTRY, 2015
+ * | 
+ * | This program is free software: you can redistribute it and/or modify
+ * | it under the terms of the GNU General Public License as published by
+ * | the Free Software Foundation, either version 3 of the License, or
+ * | any later version.
+ * |  
+ * | This program is distributed in the hope that it will be useful,
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * | GNU General Public License for more details.
+ * | 
+ * | You should have received a copy of the GNU General Public License
+ * | along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * |----------------------------------------------------------------------
+ *	
+ *	Version 1.0
+ *	 - January 09, 2015
+ *	 - first issue
+ *	
+ */
+ #include "ihome_write_private."
+ #include "ihome_public.h"
+ 
+ void ihome_write_lcd ( lcd_message_t lcd_message )
+ {
+ 
+ }
