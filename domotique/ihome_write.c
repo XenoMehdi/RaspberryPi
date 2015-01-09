f48649de2ce0d6f23d536f844e265d2a68ca10f9
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-09 11:59:20 +0000

    Update ihome_write.c

--- domotique/ihome_write.c
+++ domotique/ihome_write.c
@@ -18,5 +18,23 @@
 
 void *ihome_write ( void *prm)
 {
-
+  unsigned int l_indx ;
+  // Update outputs
+  
+  // Update message displayed on lcd's screen
+  for (l_indx = 0; l_indx < nb_OF_ACTIVE_MESSAGES ; l_indx++ )
+  {
+    // For each element of active messages, if id is different of NO_ACTIVE_MESSAGE then print message
+    if (active_message_list[l_indx].id_message != NO_ACTIVE_MESSAGE)
+    {
+      if ( active_message_list[l_indx].printed_to_lcd == FALSE )
+      {
+        ihome_write_lcd ( messages_list_cst[l_indx].lcd_message ) ;
+        sleep(0.5);
+        pthread_mutex_lock(&active_message_list[l_indx].mutex);
+        active_message_list[l_indx].printed_to_lcd = TRUE ;
+        pthread_mutex_unlock(&active_message_list[l_indx].mutex);
+      }
+    }
+  }
 }
