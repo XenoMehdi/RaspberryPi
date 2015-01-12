3d097ac1264a9b85448693a4501f41c7eb3d3d6c
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-12 15:34:06 +0000

    Update ihome_monitor.c

--- domotique/ihome_monitor.c
+++ domotique/ihome_monitor.c
@@ -13,10 +13,15 @@
 *  Written by  : E. EL FAKIR                                 Date : 07/01/2015 *
 *                                                                              *
 ********************************************************************************/
-
+#include <time.h>
 #include "ihome_public.h"
 
 void *ihome_monitor ( void *prm)
 {
+while(1)
+{
+  printf("Monitor thread\n");
+  sleep(5);
+}
 
 }
