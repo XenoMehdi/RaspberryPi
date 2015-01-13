10a05eda7ef08fb6eae2642fd3fef50c2fc50038
XenoMehdi <el.mehdi.fakir@gmail.com>
2015-01-13 15:37:55 +0000

    Update ihome_monitor.c

--- domotique/ihome_monitor.c
+++ domotique/ihome_monitor.c
@@ -1,18 +1,38 @@
-/*******************************************************************************
-* Copyright FEZ EMBEDDED SYSTEMS INDUSTRY (c) 2015       All Rights Reserved   *
-********************************************************************************/
+/**
+ *  send monitoring message to IoT server.
+ *
+ *  @Module   ihome_monitor
+ *	@author 	El Mehdi El Fakir
+ *	@email		elmehdi@elfakir.me
+ *	@website	--
+ *	@link		  --
+ *	@version 	v1.0
+ *	@compiler GCC
+ *  @hardware Raspberry Pi B+
+ *	@license	GNU GPL v3
+ *	
+ * |----------------------------------------------------------------------
+ * | Copyright (C) FEZ EMBEDDED SYSTEMS INDUSTRY, 2015
+ * | 
+ * | This program is free software: you can redistribute it and/or modify
+ * | it under the terms of the GNU General Public License as published by
+ * | the Free Software Foundation, either version 3 of the License, or
+ * | any later version.
+ * |  
+ * | This program is distributed in the hope that it will be useful,
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * | GNU General Public License for more details.
+ * | 
+ * | You should have received a copy of the GNU General Public License
+ * | along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * |----------------------------------------------------------------------
+ *	
+ *	Version 1.0
+ *	 - January 13, 2015
+ *	 - first issue
+ */
 
-/*******************************************************************************
-* PROJECT : Intelligent Home (Raspberry Pi B+)                                 *
-********************************************************************************
-*                                                                              *
-*  Module Name : ihome_monitor                                                 *
-*                                                                              *
-*  Description : send monitoring message to IoT server.                        *
-*                                                                              *
-*  Written by  : E. EL FAKIR                                 Date : 07/01/2015 *
-*                                                                              *
-********************************************************************************/
 #include "ihome_public.h"
 
 #include <stdio.h> /* printf, sprintf */
