c7b414995a7001b1929e9078a0736a5357c21e16
El Mehdi <el.mehdi.fakir@gmail.com>
2015-01-22 15:08:09 +0000

    Update ihome_write_lcd.c

--- domotique/ihome_write_lcd.c
+++ domotique/ihome_write_lcd.c
@@ -1,7 +1,7 @@
 /**
  *	write to lcd the message passed as parameter.
  *
- *  @Module   ihome_write_lcd
+ *  @Module   ihome_lcd_write
  *  @author 	El Mehdi El Fakir
  *  @email		elmehdi@elfakir.me
  *  @website	--
@@ -33,13 +33,44 @@
  *	 - first issue
  *	
  */
-// #include "ihome_write_private.h"
- #include "ihome_public.h"
+ #include "ihome_lcd.h"
+  
+ #define lcd_Write_4bits_M(cmd)     bcm2835_gpio_write(lcd_pins[2], (cmd & 0x01 != 0) ? HIGH : LOW); \
+                                    bcm2835_gpio_write(lcd_pins[3], (cmd & 0x02 != 0) ? HIGH : LOW); \
+                                    bcm2835_gpio_write(lcd_pins[4], (cmd & 0x04 != 0) ? HIGH : LOW); \
+                                    bcm2835_gpio_write(lcd_pins[5], (cmd & 0x08 != 0) ? HIGH : LOW); \ 
+                                    bcm2835_gpio_write(lcd_pins[1], HIGH); \
+                                    bcm2835_gpio_write(lcd_pins[1], LOW);
  
- void ihome_write_lcd ( lcd_message_t lcd_message )
+ #define lcd_Write_M(cmd)   bcm2835_gpio_write(lcd_pins[0], HIGH); \
+                            lcd_Write_4bits_M( (cmd & 0xf0) >> 4 ); \
+                            lcd_Write_4bits_M( (cmd & 0x0f) ); \
+                            delayMicroseconds(400);
+ 
+ #define lcd_clear_M()      bcm2835_gpio_write(lcd_pins[0], LOW);  \
+                            lcd_Write_4bits_M( 0x01 );
+ 
+ #define lcd_move_first_line _M()   bcm2835_gpio_write(lcd_pins[0], LOW); \
+                                    lcd_Write_4bits_M( 0x80 );
+ 
+ #define lcd_move_second_line _M()  bcm2835_gpio_write(lcd_pins[0], LOW); \
+                                    lcd_Write_4bits_M( 0xc0);    
+ void ihome_lcd_write ( lcd_message_t lcd_message )
  {
-  // write to each line of lcd
-//  lcdClear (lcd_handler);
-//  lcdPosition (lcd_handler, 0, 0) ; lcdPuts (lcd_handler, lcd_message.lcd_line_1 ) ;
-//  lcdPosition (lcd_handler, 0, 1) ; lcdPuts (lcd_handler, lcd_message.lcd_line_2 ) ;
+  lcd_clear_M ()
+
+// move to first line and write first message
+lcd_move_first_line _M()
+do{
+     lcd_Write_M(lcd_message.lcd_line_1[l_indx])
+ }
+ while(lcd_message.lcd_line_1[++l_indx] != '\0');
+
+ // move to second line and write second message
+ lcd_move_second_line _M()
+ do{
+    lcd_Write_M(lcd_message.lcd_line_2[l_indx])
+ }
+ while(lcd_message.lcd_line_1[++l_indx] != '\0');
+ 
  }
