bf3daf9d1cdf388f8ce983759d0d84a0dcbddad0
El Mehdi <el.mehdi.fakir@gmail.com>
2015-02-02 17:11:18 +0000

    Update ihome_initialize.c

--- domotique/ihome_initialize.c
+++ domotique/ihome_initialize.c
@@ -60,7 +60,10 @@ for ( l_indx = 0 ; l_indx < nb_Of_Input_Elements ; l_indx++ )
 {
   inputs_Array_Of_Elements [l_indx] = input_object_cst ;
 }
-
+for ( l_indx = 0 ; l_indx < nb_Of_Command_Elements ; l_indx++ )
+{
+  commands_Array_Of_Elements [l_indx] = input_object_cst ;
+}
 // initialize the output array
 for ( l_indx = 0 ; l_indx < nb_Of_Output_Elements ; l_indx++ )
 {
@@ -135,5 +138,32 @@ else
     }
   }
 }
+
+/* create the socket */
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+  {
+  	perror("ERROR opening socket");
+  	exit(0);
+  }
+
+  /* lookup the ip address */
+  server = gethostbyname(host);
+  if (server == NULL)
+  {
+  	perror("ERROR, no such host");
+  	exit(0);
+  }
+
+  /* fill in the structure */
+  memset(&serv_addr,0,sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(port);
+  memcpy(&serv_addr.sin_addr.s_addr,server->h_addr_list[0],server->h_length);
+
+  /* connect the socket */
+  if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+      error("ERROR connecting");
+
   return 0 ;
 }
